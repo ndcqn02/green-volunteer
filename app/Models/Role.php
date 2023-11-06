74a7e1860d03e07043c31b907079a2f857452141
Phan Kien <phankien110302@gmail.com>
2023-11-07 01:17:32 +0700

    add model role and user

---
+++ app/Models/Role.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+
+class Role extends Model
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        "role_Name",
+        "description"
+    ];
+    protected $table = 'role';
+}
